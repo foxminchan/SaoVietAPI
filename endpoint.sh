d788ce3799b61b2efc5eed69189da536817a248a
Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
2023-02-06 23:43:11 +0700

    edit endpoint.sh

--- endpoint.sh
+++ endpoint.sh
@@ -4,4 +4,4 @@
 
 sleep 30
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P <your_password> -Q "CREATE DATABASE SaoViet"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "Hutech@123" -Q "CREATE DATABASE SaoViet"
