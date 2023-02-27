711d3e0e15107a5e1d5bfbb92343ac4e0399be6f
Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
2023-02-27 22:25:45 +0700

    optimze import

--- Application/Middleware/RateLimitingMiddlerware.cs
+++ Application/Middleware/RateLimitingMiddlerware.cs
@@ -5,6 +5,15 @@ using System.Net;
 
 namespace Application.Middleware
 {
+    /**
+    * @Project ASP.NET Core 7.0
+    * @Author: Nguyen Xuan Nhan
+    * @Team: 4FT
+    * @Copyright (C) 2023 4FT. All rights reserved
+    * @License MIT
+    * @Create date Mon 23 Jan 2023 00:00:00 AM +07
+    */
+
     public class RateLimitingMiddleware
     {
         private readonly RequestDelegate _next;
--- WebAPI/WebAPI/Controllers/TeacherController.cs
+++ WebAPI/WebAPI/Controllers/TeacherController.cs
@@ -91,7 +91,7 @@ namespace WebAPI.Controllers
         /// <response code="500">Lỗi server</response>
         [HttpGet()]
         [AllowAnonymous]
-        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any, VaryByQueryKeys = new[] { "teachers" })]
+        [ResponseCache(Duration = 15, Location = ResponseCacheLocation.Any, VaryByQueryKeys = new[] { "teachers" })]
         public ActionResult GetTeachers()
         {
             try
